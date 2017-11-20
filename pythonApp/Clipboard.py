4894346eefce201eaf6e6447d40897b49ac2b82a
Piotr Kwiatkowski <freekwiat@gmail.com>
2017-11-20 11:44:03 +0000

    first try of pythonApp

---
+++ pythonApp/Clipboard.py
@@ -0,0 +1,11 @@
+class Clipboard(object):
+    """description of class"""
+    def __init__ (self):
+
+	def add(self):
+        print("add")
+	def get(self):
+        print("get")
+
+
+
--- pythonApp/pythonApp.py
+++ pythonApp/pythonApp.py
@@ -1,1 +1,15 @@
+import requests
+from Clipboard import Clipboard
 
+# http://dataapi20171117020514.azurewebsites.net/api/data
+
+websiteURL = 'http://localhost:2500/api/Data'
+
+r = requests.get(websiteURL)
+
+clipboard =  Clipboard()
+
+clipboard.get()
+clipboard.add()
+print (r.text)
+print (r.content)
